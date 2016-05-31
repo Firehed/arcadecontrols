12fa6f6840c6f5d375330d61ccde35bceda1dee0
Eric Stern <eric@ericstern.com>
2016-05-31 12:00:30 -0700

    Rework the config system, allow it to initialize the I2C connections

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,9 +1,33 @@
-
+use i2c;
+use i2c::{ Address, Device };
+use mcp23017::MCP23017;
 use std::fs::File;
 use std::io::Read;
 use yaml_rust::{Yaml, YamlLoader};
+use yaml_rust::yaml::Hash;
 
-pub fn load_config(path: String) -> Yaml {
+pub struct Config {
+    pub chip: MCP23017
+}
+pub fn from_file(path: String) -> Vec<Config> {
+    let yaml = parse_config_file(path);
+    let mut configs: Vec<Config> = Vec::new();
+
+    // TODO: implement this for all buses
+    match yaml["dev1"] {
+        Yaml::Hash(ref v) => {
+            let mut y = parse_bus(v, Device::Dev1);
+            configs.append(&mut y);
+        },
+        _ => {
+            println!("got nothing or wrong");
+        }
+    };
+
+    return configs;
+}
+
+fn parse_config_file(path: String) -> Yaml {
     let mut config = match File::open(path) {
         Err(e) => panic!(e),
         Ok(x) => x,
@@ -15,4 +39,26 @@ pub fn load_config(path: String) -> Yaml {
     };
     let parsed = YamlLoader::load_from_str(content.as_str()).unwrap();
     return parsed[0].clone();
+}
+
+fn parse_bus(data: &Hash, bus: Device) -> Vec<Config> {
+    // TODO: actually implement this, support all addresses
+    vec![
+        Config { chip: get_chip(bus, Address { a0: false, a1: false, a2: false }) },
+        Config { chip: get_chip(bus, Address { a0: true,  a1: true,  a2: true  }) },
+    ]
+}
+
+fn get_chip(bus: Device, address: Address) -> MCP23017 {
+    let i2c = match i2c::from_device_and_address(bus, address) {
+        Err(e) => {
+            match e {
+                i2c::Error::FileOpenError(x) => println!("Couldn't open i2c: {}", x),
+                i2c::Error::IoctlError => println!("No i2c"),
+            };
+            panic!("Could not build MCP23017");
+        },
+        Ok(value) => value,
+    };
+    return MCP23017::new(i2c);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,33 @@
 #[macro_use] extern crate nix;
 extern crate yaml_rust;
 
-use std::env;
-
 mod config;
 mod mcp23017;
 mod i2c;
 
-use i2c::{ Address, Device };
-use mcp23017::MCP23017;
+use config::Config;
+use std::env;
 
 fn main() {
 
-    let config: String = match env::args().nth(1) {
+    let config_file: String = match env::args().nth(1) {
         Some(value) => value,
         None => "config.yaml".to_string(),
     };
 
+    let mut devices = config::from_file(config_file);
 
-    let config_yaml = config::load_config(config);
-
-    let device = Device::Dev1;
-    let address = Address { a0: false, a1: false, a2: true };
-//    let path = "/dev/i2c-1";
-    let i2c = match i2c::from_device_and_address(device, address) {
-        Err(e) => {
-            match e {
-                i2c::Error::FileOpenError(x) => println!("Couldn't open i2c: {}", x),
-                i2c::Error::IoctlError => println!("No i2c"),
-            };
-            return;
-        },
-        Ok(x) => x,
-    };
-
-    let mut mcp = MCP23017::new(i2c);
-
-    for i in 1..10 {
-        let x = mcp.read();
-        println!("x {:?}", x);
+    // TODO: loop {} or daemonize or something
+    // This will become the main application loop
+    for i in 0..10 {
+        println!("{}", i);
+        for device in &mut devices {
+            poll_device(device);
+        }
     }
 }
 
-/*
-fn prepare_gpio(mut fd: &File, bus_id: MCP23017) {
-    println!("GPIO preparing");
-    fd.write_all(b"\x0a\x84");
-    fd.write_all(b"\x0a\x00");
-    let register = match bus_id {
-        MCP23017::GpioA => 0x00,
-        MCP23017::GpioB => 0x10,
-    };
-
-    let seq = [
-        register,
-        0xFF, // IODIRn   = xFF for all inputs
-        0xFF, // IPOLn    = xFF to flip inputs (makes connection to ground = 1)
-        0xFF, // GPINTENn = xFF use all pins
-        0x00, // DEFVALn  = x00 default value something...?
-        0x00, // INTCONn  = x00 compare to previous or default
-        0x84, // IOCONn   = x84 like above
-        0xFF, // GPPUn    = xFF sets resistors for inputs
-    ];
-    fd.write_all(&seq);
+fn poll_device(d: &mut Config) {
+    // This will read the chip and figure out what keypresses to simpulate depending on the result
+    println!("{:?}", d.chip.read());
 }
-
-fn read_pin(mut fd: &File, gpio: MCP23017) -> u8 {
-    // Table 1.2 from spec sheet, BANK=1
-    let address = match gpio {
-        MCP23017::GpioA => 0x09,
-        MCP23017::GpioB => 0x19,
-    };
-    fd.write_all(&[address]); // String cast of sorts
-    let mut buf = [0;1];
-    fd.read(&mut buf);
-    return buf[0];
-}
-*/
